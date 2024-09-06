cf5f673624e7129cdbda8466aff13bad29ba2924
vsh51 <volshp125@gmail.com>
2024-09-06 16:14:21 +0300

    Json parser setup with the tree-sitter project

---
+++ crates/json_parser/tests/integrational_tests.rs
@@ -0,0 +1,132 @@
+#[cfg(test)]
+mod tests {
+    use json_parser::JsonParser;
+    #[test]
+    fn test_single_pair() {
+        let code = r#"
+            {
+                "name": "John Doe",
+            }
+        "#;
+
+        let mut perser = JsonParser::default();
+        let json_value = perser.parse(code).unwrap();
+
+        let name = json_value["name"].as_str().unwrap();
+
+        assert_eq!(name, "John Doe");
+    }
+
+    #[test]
+    fn test_multiple_pairs() {
+        let code = r#"
+            {
+                "name": "John Doe",
+                "age": 30,
+                "is_student": false
+            }
+        "#;
+
+        let mut perser = JsonParser::default();
+        let json_value = perser.parse(code).unwrap();
+
+        let name = json_value["name"].as_str().unwrap();
+        let age = json_value["age"].as_number().unwrap();
+        let is_student = json_value["is_student"].as_bool().unwrap();
+
+        assert_eq!(name, "John Doe");
+        assert_eq!(age, 30.0);
+        assert!(!is_student);
+    }
+
+    #[test]
+    fn test_nested_object() {
+        let code = r#"
+            {
+                "name": "John Doe",
+                "age": 30,
+                "is_student": false,
+                "address": {
+                    "street": "123 Main St",
+                    "city": "Springfield",
+                    "state": "IL"
+                },
+            }
+        "#;
+
+        let mut perser = JsonParser::default();
+        let json_value = perser.parse(code).unwrap();
+
+        let name = json_value["name"].as_str().unwrap();
+        let age = json_value["age"].as_number().unwrap();
+        let is_student = json_value["is_student"].as_bool().unwrap();
+        let address = json_value["address"].as_object().unwrap();
+        let street = address["street"].as_str().unwrap();
+        let city = address["city"].as_str().unwrap();
+        let state = address["state"].as_str().unwrap();
+
+        assert_eq!(name, "John Doe");
+        assert_eq!(age, 30.0);
+        assert!(!is_student);
+        assert_eq!(street, "123 Main St");
+        assert_eq!(city, "Springfield");
+        assert_eq!(state, "IL");
+    }
+
+    #[test]
+    fn test_array() {
+        let code = r#"
+            {
+                "name": "John Doe",
+                "age": 30,
+                "is_student": false,
+                "children": [
+                    "Alice",
+                    "Bob"
+                ],
+            }
+        "#;
+
+        let mut perser = JsonParser::default();
+        let json_value = perser.parse(code).unwrap();
+
+        let name = json_value["name"].as_str().unwrap();
+        let age = json_value["age"].as_number().unwrap();
+        let is_student = json_value["is_student"].as_bool().unwrap();
+        let children = json_value["children"].as_array().unwrap();
+        let child1 = children[0].as_str().unwrap();
+        let child2 = children[1].as_str().unwrap();
+
+        assert_eq!(name, "John Doe");
+        assert_eq!(age, 30.0);
+        assert!(!is_student);
+        assert_eq!(child1, "Alice");
+        assert_eq!(child2, "Bob");
+    }
+
+    #[test]
+    fn test_invalid_json() {
+        let code = r#"
+            {
+                "name": "John Doe",
+                "age": 30,
+                "is_student": false,
+                "address": {
+                    "street": "123 Main St",
+                    "city": "Springfield",
+                    "state": "IL"
+                },
+                "children": [
+                    "Alice",
+                    "Bob", 
+                ],
+            }
+        "#; // Extra comma after "Bob" in array, obvious error
+
+        let mut perser = JsonParser::default();
+        let json_parse_result = perser.parse(code);
+
+        println!("{:?}", json_parse_result);
+        assert!(json_parse_result.is_err());
+    }
+}
