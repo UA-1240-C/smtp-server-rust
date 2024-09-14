374c092418500402712a9690fa51e7636fbeddfc
Roman Buchko <romanbychko84@gmail.com>
2024-09-14 10:56:06 +0300

    added an example

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,15 @@
+use std::thread;
+
 use concurrent_runtime::ConcurrentRuntime;
 use smtp_server::SmtpServer;
+use logger_proc_macro::*;
+use logger::{set_logger_level, set_logger_target, LogLevel, ConsoleLogTarget};
+use logger::info;
 
 fn main() {
+    set_logger_level(LogLevel::Trace);
+    set_logger_target(Box::new(ConsoleLogTarget));
+
     ctrlc::set_handler(move || {
         println!("received Ctrl+C!");
         println!("Shutting down server...");
@@ -12,5 +20,37 @@ fn main() {
     let mut custom_runtime = ConcurrentRuntime::new(1);
     custom_runtime.start();
     
-    SmtpServer::new(&mut custom_runtime).run();
+
+    // call the fib function
+    let result = fib(2);
+    info!("Fib result: {}", result);
+
+    // call the add function
+    custom_runtime.spawn( async {
+        let result = add(2, 3).await;
+        info!("Add result: {}", result);
+    });
+
+}
+
+#[logger(trace)]
+pub fn fib(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+#[logger(debug)]
+async fn add(a: i32, b: i32) -> i32 {
+    if a == 0 {
+        return b;
+    } 
+    if b == 0 {
+        return a;
+    }
+    return a + b;
 }
