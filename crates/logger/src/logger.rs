db382afcd87cf554f6624b42ea09822f0ba01ffe
Polina <polina_cherkasova@outlook.com>
2024-10-08 23:25:47 +0300

    feat: added file target & impl

--- crates/logger/src/logger.rs
+++ crates/logger/src/logger.rs
@@ -97,3 +97,34 @@ impl LogTarget for ConsoleLogTarget {
         }
     }
 }
+
+pub struct FileLogTarget {
+    file: File
+}
+
+impl LogTarget for FileLogTarget {
+    fn log(&self,
+           message: String
+    ) {
+        let result = writeln!(&self.file, "{}", message);
+        match result {
+            Ok(_) => {},
+            Err(_) => eprintln!("Failed to write to log file!"),
+        }
+    }
+}
+
+impl FileLogTarget {
+    pub fn new(path: &Path) -> Self {
+        let file = std::fs::OpenOptions::new().append(true).create(true).open(path);
+        match file {
+            Ok(file) => FileLogTarget { file },
+            Err(err) => {
+                eprintln!("Failed to create file: {:?}! default file is generated!", err);
+                FileLogTarget {
+                    file: std::fs::OpenOptions::new().append(true).create(true).open("serverlog_default.txt").unwrap()
+                }
+            }
+        }
+    }
+}
