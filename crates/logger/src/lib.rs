895191349b37afbd303a139014a9de621239e2d3
Polina <polina_cherkasova@outlook.com>
2024-10-09 13:01:01 +0300

    feat: added static queue, fixed interactions with logger & queue

--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -1,61 +1,74 @@
 mod logger;
 
-use std::sync::{Arc, LazyLock};
+use crossbeam_queue::ArrayQueue;
+use std::any::Any;
+use std::{
+    io::{IsTerminal, Write},
+    sync::Arc
+};
+use std::ops::Deref;
+use std::sync::LazyLock;
 pub use logger::*;
 mod logger_macro;
 mod writer;
 mod queue;
 
-static LOGGER : LazyLock<Arc<Logger>> = LazyLock::new(||{Arc::new(Logger::new(LogLevel::Trace, "serverlog.txt".to_string()))});
+const DEFAULT_LOG_CAPACITY: usize = 1000;
 
+static LOGGING_QUEUE: LazyLock<Arc<ArrayQueue<LogMessage>>> = LazyLock::new(||{
+    Arc::new(ArrayQueue::new(DEFAULT_LOG_CAPACITY)
+    )});
 
-pub fn initialize_logger(severity_level: LogLevel, filename: String) {
-    update_severity_level(severity_level);
+static mut LOGGER : LazyLock<Arc<Logger>> = LazyLock::new(||{Arc::new(Logger::new(LogLevel::Info,
+                                                                              DEFAULT_LOG_CAPACITY,
+                                                                              Box::new(ConsoleLogTarget)))});
+pub fn initialize_logger(
+    severity_level: LogLevel,
+    queue_capacity: usize,
+    target: Box<dyn LogTarget + Send + Sync>,
+) {
+    unsafe {
+        LOGGER.update_severity_level(severity_level);
+        LOGGER.update_queue_capacity(queue_capacity);
+        LOGGER.add_target(target);
+    }
     start_consumer_thread();
 }
 
-pub fn update_severity_level(severity_level: LogLevel) {
-    LOGGER.update_severity_level(severity_level);
-}
-
 pub fn log(level: LogLevel, message: String) {
-    LOGGER.log(level, message);
+    unsafe { LOGGER.log(level, message); }
 }
 
 pub fn log_debug(message: String) {
-    LOGGER.log(LogLevel::Debug, message);
+    unsafe { LOGGER.log(LogLevel::Debug, message); }
 }
 
 pub fn log_prod(message: String) {
-    LOGGER.log(LogLevel::Info, message);
+    unsafe { LOGGER.log(LogLevel::Info, message); }
+}
+
+pub fn log_prod_error(message: String) {
+    unsafe { LOGGER.log(LogLevel::Error, message); }
 }
 
 pub fn log_warn(message: String) {
-    LOGGER.log(LogLevel::Warn, message);
-}
-
-pub fn log_error(message: String) {
-    LOGGER.log(LogLevel::Error, message);
+    unsafe { LOGGER.log(LogLevel::Warn, message); }
 }
 
 pub fn log_trace(message: String) {
-    LOGGER.log(LogLevel::Trace, message);
+    unsafe { LOGGER.log(LogLevel::Trace, message); }
 }
 
 pub fn flush() {}
 
 pub fn terminate() {
-    LOGGER.shutdown();
+    unsafe { LOGGER.shutdown(); }
 }
 
-pub fn set_logger_level(severity_level: LogLevel) {
-    LOGGER.update_severity_level(severity_level);
+pub fn set_logger_target(target: Box<dyn LogTarget + Send + Sync>) {
+    unsafe { LOGGER.add_target(target); }
 }
-
-pub fn set_logger_target(target: Box<dyn LogTarget + Send + Sync>) {}
-
-pub fn set_logger_cache_capacity(capacity: usize) {}
 
 pub fn get_logger_level() -> LogLevel {
-    LOGGER.get_log_level()
+    unsafe { LOGGER.get_log_level() }
 }
